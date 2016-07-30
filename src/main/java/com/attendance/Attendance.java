44641ba9096bdaf989643edacc8c9eac1d2c160d
indrajeetlatthe <indrajeet183@gmail.com>
2016-07-30 22:49:22 +0530

    ID not auto generating

--- src/main/java/com/attendance/Attendance.java
+++ src/main/java/com/attendance/Attendance.java
@@ -11,12 +11,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @Entity
 public class Attendance {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
+    private long Id;
 
     private String branchName;
 
@@ -26,12 +28,13 @@ public class Attendance {
     public void setbranchName(String branchName) {
         this.branchName=branchName;
     }
+    public void setId(long Id){this.Id=Id;}
     public String getbranchName(String branchName){
         return branchName;
     }
     @Override
     public String toString() {
-        return String.format("ID :"+id+" BranchName :" +branchName);
+        return String.format("ID :"+Id+" BranchName :" +branchName);
     }
     protected Attendance() {}
 
